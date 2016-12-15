de81289d041534fd2162723cb6dac3a74e257b8f
Aaron Mann <aaronmann613348@gmail.com>
2016-12-15 16:29:27 -0600

    Initial Commit

---
+++ local_1Tests/local_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  local_1Tests.swift
+//  local_1Tests
+//
+//  Created by Aaron Mann on 12/15/16.
+//  Copyright © 2016 Aaron Mann. All rights reserved.
+//
+
+import XCTest
+@testable import local_1
+
+class local_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
